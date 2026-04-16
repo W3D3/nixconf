0e57412d916c4645a4098aa7b78b950918997086
Christoph Wedenig <christoph@wedenig.org>
2026-04-16 20:23:55 +0200

    email stuff

--- modules/home/wedenigc.nix
+++ modules/home/wedenigc.nix
@@ -142,12 +142,23 @@
 
             programs.thunderbird = {
               enable = true;
-              profiles.default.isDefault = true;
+              profiles.default = {
+                isDefault = true;
+                extensions = [
+                  (pkgs.fetchFirefoxAddon {
+                    name = "thunderai";
+                    url = "https://github.com/micz/ThunderAI/releases/download/v4.0.6/thunderai-v4.0.6.xpi";
+                    sha256 = "sha256-86wuKdsFUTcIpg7k6XAbwKbZnopjT24pmrlCV46OOok=";
+                    fixedExtid = "thunderai@micz.it";
+                  })
+                ];
+              };
             };
 
             accounts.email.accounts.gmail = {
-              name = "Gmail";
+              primary = true;
               address = "wedenigc@gmail.com";
+              userName = "wedenigc@gmail.com";
               realName = "Christoph Wedenig";
               thunderbird = {
                 enable = true;
@@ -166,7 +177,6 @@
             };
 
             accounts.email.accounts.aau = {
-              name = "AAU Mail";
               address = "christoph.wedenig@aau.at";
               realName = "Christoph Wedenig";
               userName = "christoph.wedenig@aau.at";
