df5087d75a78038de838cb445627a361934967fc
Christoph Wedenig <christoph@wedenig.org>
2026-08-08 23:16:58 +0200

    Remove Garnix cache from Telegram feature config

--- modules/features/telegram.nix
+++ modules/features/telegram.nix
@@ -8,11 +8,9 @@
       nix.settings = {
         extra-substituters = [
           "https://ayugram-desktop.cachix.org"
-          "https://cache.garnix.io"
         ];
         extra-trusted-public-keys = [
           "ayugram-desktop.cachix.org-1:AZ5EqHrJsAKL5YkZYLPEsb1FdD9QlypUwQ0REcJftgA="
-          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         ];
       };
 
